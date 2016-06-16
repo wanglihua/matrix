5c520b197995bdaa3ddf532289d699ccdff85e74
wanglihua <wanglihuahaiyan@qq.com>
2016-06-16 18:39:04 +0800

    layout

--- main.go
+++ main.go
@@ -14,32 +14,29 @@ import (
     "path/filepath"
     "os"
     "log"
-    "fmt"
 )
 
 var (
-    runMode    *string = flag.String("runMode", "", "Run mode.")
+    runMode    *string = flag.String("runMode", "dev", "Run mode.")
     port       *int = flag.Int("port", 0, "By default, read from app.conf")
-    importPath *string = flag.String("importPath", "", "Go Import Path for the app.")
+    importPath *string = flag.String("importPath", "matrix", "Go Import Path for the app.")
     srcPath    *string = flag.String("srcPath", "", "Path to the source root.")
 
     _ = reflect.Invalid
 )
 
 func main() {
-    flag.Parse()
 
-    if revel.DevMode == false {
-        //部署的时候自动把"matrix" 作为 importPath，把当前目录作为 srcPath
-        fmt.Println("not devmode")
-
-        *importPath = "matrix"
+    if *runMode != "dev" {
+        //部署的时候把当前目录下的src目录作为 srcPath
         currentDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
         if err != nil {
             log.Fatal(err)
         }
         *srcPath = currentDir + "\\src";
     }
+
+    flag.Parse()
 
     revel.Init(*runMode, *importPath, *srcPath)
     revel.INFO.Println("Running revel server")
