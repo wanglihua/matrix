8c519f748b2118cf1ecee8854defdc09120c3147
wanglihua <wanglihuahaiyan@qq.com>
2016-06-21 07:00:01 +0800

    grid request param

--- serice/gridrequest/grid_request.go
+++ serice/gridrequest/grid_request.go
@@ -3,9 +3,34 @@ package GridRequest
 import (
     "github.com/revel/revel"
     "strconv"
+    "fmt"
 )
 
 func GetParam(request *revel.Request) (filter string, order string, offset int, limit int) {
+
+    searchValue := request.Form["sSearch"][0]
+
+    fmt.Println(searchValue)
+
+    colCount, _ := strconv.Atoi(request.Form["iColumns"][0])
+    for colIndex := 0; colIndex < colCount; colIndex++ {
+        colName := request.Form["mDataProp_" + strconv.Itoa(colIndex)][0]
+        colSearchable := request.Form["bSearchable_" + strconv.Itoa(colIndex)][0]
+        colSortable := request.Form["bSortable_" + strconv.Itoa(colIndex)][0]
+
+        fmt.Println(colName)
+        fmt.Println(colSearchable)
+        fmt.Println(colSortable)
+    }
+
+    orderCount, _ := strconv.Atoi(request.Form["iSortingCols"][0])
+    for orderIndex := 0; orderIndex < orderCount; orderIndex++ {
+        orderColIndex, _ := strconv.Atoi(request.Form["iSortCol_" + strconv.Itoa(orderIndex)][0])
+        orderDir := request.Form["sSortDir_" + strconv.Itoa(orderIndex)][0]
+
+        fmt.Println(orderColIndex)
+        fmt.Println(orderDir)
+    }
 
     filter = ""
     order = ""
