b8cd7e31f14fd843b6fc7750997a5e67fdf0565b
Richard.Wang <Richard.Wang@linkores.com>
2016-06-28 13:10:48 +0800

    reply

---
+++ modules/wechat/app/controllers/callback.go
@@ -0,0 +1,122 @@
+package controllers
+
+import (
+    "encoding/xml"
+    "github.com/revel/revel"
+    matrixService "matrix/service"
+    wechatService "matrix/modules/wechat/service"
+    "fmt"
+    "io/ioutil"
+    "encoding/base64"
+)
+
+type WechatCallback struct {
+    *revel.Controller
+    matrixService.BaseController
+}
+
+func (c WechatCallback) Sign() revel.Result {
+    var signature, timestamp, nonce, echostr string
+
+    c.Params.Bind(&signature, "signature")
+    c.Params.Bind(&timestamp, "timestamp")
+    c.Params.Bind(&nonce, "nonce")
+    c.Params.Bind(&echostr, "echostr")
+
+    if echostr != "" {
+        signatureComputed := wechatService.Sign(wechatService.GetToken(), timestamp, nonce)
+        if signature == signatureComputed {
+            return c.RenderText(echostr)
+        }
+    }
+    return c.RenderText("")
+}
+
+func (c WechatCallback) Reply() revel.Result {
+    var signature, msgSignature, encrypt_type, timestamp, nonce string
+
+    c.Params.Bind(&encrypt_type, "encrypt_type")
+    c.Params.Bind(&signature, "signature")
+    c.Params.Bind(&msgSignature, "msg_signature")
+    c.Params.Bind(&timestamp, "timestamp")
+    c.Params.Bind(&nonce, "nonce")
+
+    fmt.Println(encrypt_type)
+    fmt.Println(signature)
+    fmt.Println(msgSignature)
+    fmt.Println(timestamp)
+    fmt.Println(nonce)
+
+    signatureComputed := wechatService.Sign(wechatService.GetToken(), timestamp, nonce)
+    if signature != signatureComputed {
+        fmt.Println("signature != signatureComputed")
+        return c.RenderText("")
+    }
+
+    /*
+    bodyBuffer := new(bytes.Buffer)
+    bodyBuffer.ReadFrom(c.Request.Body)
+    msgXml := bodyBuffer.String()
+    fmt.Println(msgXml)
+    */
+
+    msgRequestBody, _ := ioutil.ReadAll(c.Request.Body)
+    msg := new(Message)
+    xml.Unmarshal(msgRequestBody, msg)
+
+    msgSignatureComputed := wechatService.MsgSign(wechatService.GetToken(), timestamp, nonce, msg.Encrypt)
+    if msgSignature != msgSignatureComputed {
+        return c.RenderText("")
+    }
+
+    fmt.Println(msg.ToUserName)
+    fmt.Println(msg.Encrypt)
+
+    //msg.Encrypt base64 decode
+    msgEncryptByte := []byte(msg.Encrypt)
+    encryptedMsg := make([]byte, base64.StdEncoding.DecodedLen(len(msgEncryptByte)))
+    encryptedMsgLen, _ := base64.StdEncoding.Decode(encryptedMsg, msgEncryptByte)
+    encryptedMsg = encryptedMsg[:encryptedMsgLen]
+
+    fmt.Println(encryptedMsg)
+
+    random, msgPlaintext, haveAppIdBytes, _ :=  wechatService.AesDecryptMsg(encryptedMsg, []byte(wechatService.GetAesKey()))
+    fmt.Println(random)
+    fmt.Println(msgPlaintext)
+    fmt.Println(haveAppIdBytes)
+
+    return c.RenderText("")
+}
+
+/*
+<xml>
+    <ToUserName>...</ToUserName>
+    <Encrypt>...</Encrypt>
+</xml>
+*/
+type Message struct {
+    ToUserName string  `xml:"ToUserName"`
+    Encrypt    string  `xml:"Encrypt"`
+}
+
+
+/*
+package main
+
+import "fmt"
+import "crypto/aes"
+
+func main() {
+    bc, err := aes.NewCipher([]byte("key3456789012345"))
+    if (err != nil) {
+        fmt.Println(err);
+    }
+    fmt.Printf("The block size is %d\n", bc.BlockSize())
+
+    var dst = make([]byte, 16)
+    var src = []byte("sensitive1234567")
+
+    bc.Encrypt(dst, src)
+    fmt.Println(dst)
+}
+ */
---
+++ modules/wechat/service/config.go
@@ -0,0 +1,19 @@
+package service
+
+var token = "matrix_dev_token"
+var appId = ""
+var appSecret = ""
+var aesKey = "bTmRqUZk8WGdImCAqN45u0Hw5LngZb983HGqZF5KLDE"
+
+func GetToken() string {
+    return token
+}
+
+func GetAppId() string {
+    return appId
+}
+
+func GetAesKey() string {
+    return aesKey
+}
+
