0c8f0799eebb57fc71635a8ec4495baa338d5685
wanglihua <wanglihuahaiyan@qq.com>
2016-07-22 14:32:41 +0800

    jobs

---
+++ modules/jobs/app/controllers/status.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+    "strings"
+
+    "github.com/revel/cron"
+    "github.com/revel/modules/jobs/app/jobs"
+    "github.com/revel/revel"
+)
+
+type Jobs struct {
+    *revel.Controller
+}
+
+func (c Jobs) Status() revel.Result {
+    remoteAddress := c.Request.RemoteAddr
+    //revel.TRACE.Println(remoteAddress)
+    if revel.Config.BoolDefault("jobs.acceptproxyaddress", false) {
+        if proxiedAddress, isProxied := c.Request.Header["X-Forwarded-For"]; isProxied {
+            remoteAddress = proxiedAddress[0]
+        }
+    }
+    if !strings.HasPrefix(remoteAddress, "127.0.0.1") && !strings.HasPrefix(remoteAddress, "::1") &&
+       !strings.HasPrefix(remoteAddress, "[127.0.0.1]") && !strings.HasPrefix(remoteAddress, "[::1]") {
+        return c.Forbidden("%s is not local", remoteAddress)
+    }
+
+    entries := jobs.MainCron.Entries()
+    c.RenderArgs["entries"] = entries
+    return c.RenderTemplate("jobs/status.html")
+}
+
+func init() {
+    revel.TemplateFuncs["castjob"] = func(job cron.Job) *jobs.Job {
+        return job.(*jobs.Job)
+    }
+}
---
+++ modules/jobs/app/plugin.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+    //"fmt"
+    "github.com/revel/revel"
+    //"time"
+    "matrix/modules/jobs/app/jobs"
+)
+
+func init() {
+    revel.OnAppStart(func() {
+        //fmt.Println("jobs module start!")
+
+        /*
+        jobs.Schedule("0 0 0 * * ?",  TestTask{})
+        jobs.Schedule("@midnight",    TestTask{})
+        jobs.Schedule("@every 24h",   TestTask{})
+        jobs.Every(24 * time.Hour,    TestTask{})
+        */
+
+        //cron.test = * * * * * ?
+        jobs.Schedule("cron.test", TestTask{})
+    })
+}
+
+
+type TestTask struct {
+}
+
+func (e TestTask) Run() {
+    revel.TRACE.Println("这是一个定时任务的测试！")
+}
