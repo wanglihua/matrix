c9aab56eec4c53d046f90924efbd87f08f1f0525
wanglihua <wanglihuahaiyan@qq.com>
2016-08-21 21:51:19 +0800

    qrcode

---
+++ core/qrcode.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
+)
+
+func Create_qrcode(data string) barcode.Barcode {
+
+	code, err := qr.Encode(data, qr.L, qr.Unicode)
+	HandleError(err)
+
+	if data != code.Content() {
+		panic("Create_qrcode data differs")
+	}
+
+	code, err = barcode.Scale(code, 300, 300)
+	HandleError(err)
+
+	return code
+}
+
+
---
+++ core/qrcode_test.go
@@ -0,0 +1,24 @@
+package core
+
+import (
+	"os"
+	"image"
+	"image/png"
+	"testing"
+)
+
+func Test_create_qrcode(t *testing.T) {
+	code := Create_qrcode("123456")
+	write_png("z:/qrcode_123456.png", code)
+}
+
+func write_png(file_name string, img image.Image) {
+	file, err := os.Create(file_name)
+	HandleError(err)
+
+	err = png.Encode(file, img)
+	// err = jpeg.Encode(file, img, &jpeg.Options{100})      //图像质量值为100，是最好的图像显示
+	HandleError(err)
+
+	file.Close()
+}
