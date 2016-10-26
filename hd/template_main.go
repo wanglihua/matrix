c10119ffed7c101cd5b72b782bf0825e9c095c97
wanglihua <wanglihuahaiyan@qq.com>
2016-10-26 22:39:27 +0800

    window service

--- hd/template_main.go
+++ hd/template_main.go
@@ -15,15 +15,19 @@ import (
     "{{.ImportPath}}/app"
 	"{{.ImportPath}}/core/lic"
     "{{.ImportPath}}/db"
+	"{{.ImportPath}}/core/winsvc"
     "fmt"
     "strconv"
+	"golang.org/x/sys/windows/svc"
+	"strings"
 )
 
 var (
-    runMode    *string = flag.String("runMode", "prod", "Run mode.")
-    port       *int    = flag.Int("port", 0, "By default, read from app.conf")
-    importPath *string = flag.String("importPath", "{{.ImportPath}}", "Go Import Path for the app.")
-    srcPath    *string = flag.String("srcPath", "", "Path to the source root.")
+	service_action *string = flag.String("service", "", "windows service action to perform: install remove start stop pause continue")
+	runMode        *string = flag.String("runMode", "prod", "Run mode: dev prod")
+	port           int = 0
+	importPath     string = "matrix"
+	srcPath        string = ""
 
     // So compiler won't complain if the generated code doesn't reference reflect package...
     _ = reflect.Invalid
@@ -46,14 +50,14 @@ func main() {
     lic_file_path := currentDir + string(os.PathSeparator) + "app.lic"
 
     if *runMode == "prod" {
-        *importPath = "{{.ImportPath}}"
-        *srcPath = currentDir
+        importPath = "{{.ImportPath}}"
+        srcPath = currentDir
         lic_file_path = currentDir + string(os.PathSeparator) + "{{.ImportPath}}" + string(os.PathSeparator) + "app.lic"
     }
 
     lic.ValidAppLic(app.AppName, lic_file_path)
 
-    revel.Init(*runMode, *importPath, *srcPath)
+    revel.Init(*runMode, importPath, srcPath)
     revel.INFO.Println("Running revel server")
 
     {{range $i, $c := .Controllers}}
@@ -83,25 +87,42 @@ func main() {
     }
 
 
-    db.InitDatabase()
+	isIntSess, err := svc.IsAnInteractiveSession()
+	if err != nil {
+		log.Fatalf("failed to determine if we are running in an interactive session: %v", err)
+	}
+	if isIntSess == false {
+		run_web()
+	}
 
-    /*
-    revel.HttpAddr = "127.0.0.1"
-    var err error
-    db.Engine, err = xorm.NewEngine("sqlite3", "./app.db")
-    if err != nil  {
-        panic(err)
-    }
-    */
+	if strings.TrimSpace(*service_action) == "" {
+		run_web()
+	} else if *service_action == "install" {
+		winsvc.InstallService(app.AppName, app.AppName)
+	} else if *service_action == "remove" {
+		winsvc.RemoveService(app.AppName)
+	} else if *service_action == "start" {
+		winsvc.StartService(app.AppName)
+	} else if *service_action == "stop" {
+		winsvc.StopService(app.AppName)
+	} else if *service_action == "pause" {
+		winsvc.PauseService(app.AppName)
+	} else if *service_action == "continue" {
+		winsvc.ContinueService(app.AppName)
+	}
+}
 
-    if revel.DevMode == false {
-        if revel.HttpPort == 80 {
-            fmt.Println("请打开浏览器访问 http://localhost")
-        } else {
-            fmt.Println("请打开浏览器访问 http://localhost:" + strconv.Itoa(revel.HttpPort))
-        }
-    }
+func run_web() {
+	db.InitDatabase()
 
-    revel.Run(*port)
+	if revel.DevMode == false {
+		if revel.HttpPort == 80 {
+			fmt.Println("请打开浏览器访问 http://localhost")
+		} else {
+			fmt.Println("请打开浏览器访问 http://localhost:" + strconv.Itoa(revel.HttpPort))
+		}
+	}
+
+	revel.Run(port)
 }
 `
