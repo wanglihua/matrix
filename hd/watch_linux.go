ce641c4b1013c697d4bd0aaa138d5f1bf52dc3b0
wanglihua <wanglihuahaiyan@qq.com>
2016-11-01 20:29:23 +0800

    hd

--- hd/watch_linux.go
+++ hd/watch_linux.go
@@ -206,7 +206,7 @@ func Start(appname string) {
 	cmd = exec.Command(appname)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Args = append([]string{appname}, "service debug")
+	cmd.Args = append([]string{appname}, "service", "debug")
 
 	go cmd.Run()
 	ColorLog("[INFO] %s is running...\n", appname)
