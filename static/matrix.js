a5b17a856a7b9a490eee111b08a4cadaabbabf9d
wanglihua <wanglihuahaiyan@qq.com>
2016-11-13 09:45:18 +0800

    element

--- static/matrix.js
+++ static/matrix.js
@@ -1,3 +1,24 @@
-/**
- * Created by wanglihua on 16-11-12.
- */
+function handleJQueryAjaxError(xhr, textStatus, error) {
+    if (textStatus === 'timeout') {
+        $.msg.error('服务器超时！');
+    }
+    else {
+        $.msg.error('访问服务器出错，请重试！' + error);
+    }
+}
+
+$.ajaxSetup(
+    {
+        type: "POST",
+        "timeout": 15000,
+        "error": handleJQueryAjaxError
+    }
+);
+
+function isEmptyStr(str) {
+    if (!str || str.length === 0 || str === "" || typeof str == 'undefined' || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g, "") === "") {
+        return true;
+    } else {
+        return false;
+    }
+}
