63bc712cadac43929756794078a529f46a8288a2
wanglihua <wanglihuahaiyan@qq.com>
2016-12-26 22:13:02 +0800

    storage loc

---
+++ modules/erp/models/StorageLocInfo.Meta.go
@@ -0,0 +1,25 @@
+package models
+
+var StorageLocCols = struct {
+	Id string
+	Name string
+	Desc string
+	XAxes string
+	YAxes string
+	Phone string
+	StockId string
+	CreateTime string
+	UpdateTime string
+	Version string
+}{
+	Id: "id",
+	Name: "name",
+	Desc: "desc",
+	XAxes: "x_axes",
+	YAxes: "y_axes",
+	Phone: "phone",
+	StockId: "stock_id",
+	CreateTime: "create_time",
+	UpdateTime: "update_time",
+	Version: "version",
+}
---
+++ modules/erp/models/StorageLocInfo.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"matrix/core"
+)
+
+type StorageLocInfo struct {
+	Id         int64                 `xorm:"bigint notnull pk autoincr 'id'" json:"id"`
+	Name       string                `xorm:"nvarchar(200) notnull index 'name'" json:"name" smith:"verbose_name=名称,min=2,max=100"`
+	Desc       core.NullString       `xorm:"nvarchar(500) 'desc'" json:"desc" smith:"verbose_name=描述,max=500"`
+	XAxes      string                `xorm:"nvarchar(100) 'x_axes'" json:"x_axes" smith:"verbose_name=X轴坐标,max=100"`
+	YAxes      string                `xorm:"nvarchar(100) 'y_axes'" json:"y_axes" smith:"verbose_name=Y轴坐标,max=100"`
+	Phone      core.NullString       `xorm:"nvarchar(100) 'phone'" json:"phone" smith:"verbose_name=电话,min=2,max=100"`
+	StockId    int64                 `xorm:"bigint notnull index 'stock_id'" json:"stock_id" smith:"verbose_name=仓库"`
+	CreateTime core.Time             `xorm:"created notnull 'create_time'" json:"create_time"`
+	UpdateTime core.Time             `xorm:"updated notnull 'update_time'" json:"update_time"`
+	Version    int                   `xorm:"version notnull 'version'" json:"version"`
+}
+
+func (e StorageLocInfo) TableName() string {
+	return TablePrefix + "storage_loc"
+}
+
+func (e StorageLocInfo) ModelDesc() string {
+	return "verbose_name=仓位,entity_json=storage_loc,route=storage/loc"
+}
