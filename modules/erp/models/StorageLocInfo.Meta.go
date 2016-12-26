fb2a5680a1b6447a36d64dadadcc4a651a1ccfa9
wanglihua <wanglihuahaiyan@qq.com>
2016-12-27 06:39:18 +0800

    meta

---
+++ modules/erp/models/StorageLocInfo.Meta.go
@@ -0,0 +1,23 @@
+package models
+
+var StorageLocCols = struct {
+	Id string
+	StockId string
+	Name string
+	Desc string
+	XAxes string
+	YAxes string
+	CreateTime string
+	UpdateTime string
+	Version string
+}{
+	Id: "id",
+	StockId: "stock_id",
+	Name: "name",
+	Desc: "desc",
+	XAxes: "x_axes",
+	YAxes: "y_axes",
+	CreateTime: "create_time",
+	UpdateTime: "update_time",
+	Version: "version",
+}
